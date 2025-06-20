d3e5bda0cfae11af4b771883e4f0c1238b764eff
CyberAshu <mr.ayushsen@gmail.com>
2025-06-20 12:34:02 +0530

    Switch database backend from PostgreSQL to SQLite for local development

--- aaradhyadhrma/settings.py
+++ aaradhyadhrma/settings.py
@@ -128,17 +128,8 @@ WSGI_APPLICATION = 'aaradhyadhrma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',
-            'connect_timeout': 10,
-            'options': '-c statement_timeout=15000',  # 15 seconds
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
